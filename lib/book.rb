49853da3b34778c8174952cdaeb4c95d8a624cef
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-25 14:39:22 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+# run learn spec/01_book_spec.rb
 
+class Book
 
+    attr_accessor :author, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def page_count=(num)
+        @page_count = num
+    end
+     
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+# run learn spec/02_shoe_spec.rb
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # def brand=(new_brand)
+    #     @brand = new_brand
+    # end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
